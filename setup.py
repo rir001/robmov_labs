6f4bb87829864596dcfd4276c43519de11ea3337
rir001 <adrivera1@puc.cl>
2024-04-21 22:53:36 -0400

    added launch to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'robmov_labs'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.xml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
