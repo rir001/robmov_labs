10ab88b44228e0acf6e85f40392167bfb711c70a
rir001 <riveraldrv@gmail.com>
2024-06-29 17:56:05 -0400

    o

--- robmov_labs/localization.py
+++ robmov_labs/localization.py
@@ -7,19 +7,72 @@ import rclpy
 from threading import Thread
 from rclpy.node import Node
 from std_msgs.msg import Float64
-from tf_transformations import euler_from_quaternion
-from geometry_msgs.msg import Twist, PoseArray, Pose
+from tf_transformations import euler_from_quaternion, quaternion_from_euler
+from geometry_msgs.msg import Twist, PoseArray, Pose, Point, Quaternion
 from sensor_msgs.msg import Image, LaserScan
 from cv_bridge import CvBridge
 
+import cv2
 import matplotlib.pyplot as plt
 
-import cv2
-import random
+import os
+
 
 VEL = 0.4
 PLOT = 1
-PARTICLES = 100
+PARTICLES = 100_000
+
+SCALE = 100
+RANGO_ANGULAR = np.arange(-np.pi/2, np.pi/2, 0.01745329238474369)
+
+REAL_MAP = cv2.imread("./src/robmov_labs/maps/mapa.pgm", cv2.IMREAD_GRAYSCALE)
+
+maskm_map = REAL_MAP.copy()
+
+maskm_map[maskm_map == 0] = 255
+maskm_map[maskm_map == 205] = 0
+maskm_map = cv2.erode(maskm_map, np.ones([19]*2, np.uint8), iterations=1)
+maskm_map = (np.logical_not(REAL_MAP).astype(np.uint8) * maskm_map / 255).astype(np.uint8)
+DARK_MAP = cv2.dilate(maskm_map, np.ones([3]*2, np.uint8), iterations=1)
+
+
+def get_particles(N:int, points=[], r:int=10, angle_tolerance:int=0.1) -> np.ndarray:
+    angles = np.linspace(-np.pi, np.pi, int(360/angle_tolerance))
+
+    if list(points) == []:
+        spaces = np.column_stack(np.where(REAL_MAP == 255))
+    else:
+        options = np.zeros(REAL_MAP.shape, dtype=np.uint8)
+        for point in points: options[int(point[0])-r:int(point[0])+r+1, int(point[1])-r:int(point[1])+r+1] = 1
+        spaces = np.column_stack(np.where((options * REAL_MAP) == 255))
+
+    return np.column_stack([
+            spaces[np.random.randint(spaces.shape[0] , size=N), :],
+            np.random.choice(angles, size=N),
+        ])
+
+def get_more_correct_particles(particles:list, muestra:list, tolerance:int=1, umbral:int=3):
+    sobrevivientes = []
+
+    for y, x, a in particles:
+        N = 0
+        for n in range(62, 119):
+            y_a = int(y - SCALE*muestra[n]*np.sin(RANGO_ANGULAR[n] + a))
+            x_a = int(x + SCALE*muestra[n]*np.cos(RANGO_ANGULAR[n] + a))
+
+            if 0 < y_a < DARK_MAP.shape[0] and 0 < x_a < DARK_MAP.shape[1]:
+                if np.sum(DARK_MAP[y_a-tolerance:y_a+tolerance+1, x_a-tolerance:x_a+tolerance+1]) == 0:
+                    N += 1
+                    if N > umbral:
+                        break
+            else:
+                N = 70
+                break
+        if N <= umbral:
+            sobrevivientes.append([y, x, a])
+
+    return np.array(sobrevivientes)
+
 
 class Localization( Node ):
 
@@ -27,24 +80,14 @@ class Localization( Node ):
         super().__init__( 'localization' )
 
         self.ploted = False
-        self.particles = self.initialize_particles( PARTICLES )
+
 
         self.lidar_sub          = self.create_subscription( LaserScan, '/scan', self.process_lidar,  1 )
         self.cmd_vel_mux_pub    = self.create_publisher( Twist, '/cmd_vel_mux/input/navigation', 10 )
+        self.particle_pub       = self.create_publisher( PoseArray, 'particles', 1 )
 
         self.bg = CvBridge()
 
-
-    def initialize_particles(self, num_particles):
-        particles = []
-        for _ in range(num_particles):
-            particle = Pose()
-            particle.position.x = random.uniform(0, 10)
-            particle.position.y = random.uniform(0, 10)
-            particle.orientation.w = 1.0
-            particles.append(particle)
-        return particles
-    
 
     def show_camera(self, image):
         self.get_logger().info( "Camera" )
@@ -53,7 +96,6 @@ class Localization( Node ):
 
         cv2.imshow("camera", cv_camera)
         cv2.waitKey(1)
-
 
     def load_plot(self, rango):
         plt.ion()
@@ -67,31 +109,39 @@ class Localization( Node ):
         ax.grid(True)
         self.ploted = True
 
-
     def show_lidar(self, image):
         if not self.ploted: self.load_plot(np.arange(image.angle_min, image.angle_max, image.angle_increment))
         self.line.set_ydata(image.ranges)
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
-        self.get_logger().info( f"{ image.ranges }" )
+        # self.get_logger().info( f"{ image.ranges }" )
+
+    def publish_particles(self, particles):
+        poses = []
+        for p in particles:
+            q = quaternion_from_euler(0, 0, p[2])
+            poses.append(Pose(
+                position = Point(x=float(p[0]/SCALE), y=float(p[1]/SCALE), z=float(0)),
+                orientation = Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
+            ))
+        msg = PoseArray()
+        msg.poses = poses
+        self.particle_pub.publish(msg)
+
 
     def process_lidar(self, image):
         if PLOT: self.show_lidar(image)
 
-        try:
-            # Procesamos la imagen para obtner el mapa visto por el robot
-            fixed_map, angle = correct_angle(image.ranges)
-            # self.get_logger().info( f"Angle: {angle}" )
-            # self.get_logger().info( f"Fixed Map: {fixed_map}" )
-        except:
-            self.get_logger().info("Error")
+        particles = get_particles(PARTICLES)
+        live_particles = get_more_correct_particles(particles, list(image.ranges), tolerance=3, umbral=5)
+        particles = np.vstack([
+            get_particles(int(PARTICLES*0.95), points=live_particles, angle_tolerance=0.001),
+            get_particles(int(PARTICLES*0.05)),
+        ])
+        live_particles = get_more_correct_particles(particles, list(image.ranges), tolerance=1, umbral=1)
+        self.get_logger().info( f"{ live_particles }" )
+        self.publish_particles(live_particles)
 
-        # Luego, hacemos map matching para obtener la posición del robot en el mapa
-        mapa = cv2.imread("maps/mapa.png", cv2.IMREAD_GRAYSCALE)
-
-        # Finalmente, publicamos la posición del robot en el mapa
-
-        # Meterle un contador, para que despues de x iteraciones, cree particulas nuevas (evitar errores)
 
 
 
@@ -105,72 +155,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# UTILS
-
-def correct_angle(muestra):
-    start_angle = -np.pi/2
-    final_angle =  np.pi/2
-    step = 0.01745329238474369
-
-    rango = np.arange(start_angle, final_angle, step)
-
-    scale = 400
-    base = np.ones((2*2*scale, 4*scale), dtype=np.uint8) * 255
-    points = []
-
-    for n in range(62, 119):
-        if muestra[n] == 4.0:
-            continue
-        x = int(muestra[n]*np.cos(rango[n])*scale)
-        y = int(muestra[n]*np.sin(rango[n])*scale)
-        base[-y + 2*scale, x] = 0
-        points.append((x, y))
-    
-    h = get_image_angle(points)
-    for n in range(len(h)):
-        if h[n] < 0:
-            h[n] = h[n] + np.pi
-    
-    angle_ = find_more_common_angle(h)
-
-    basee_point = get_point_with_angle(points, angle_)[0]
-
-    fixed_map = np.ones((2*2*scale, 4*scale), dtype=np.uint8)
-
-    for p in points:
-        x, y = p
-        ang = get_angle(basee_point, p)
-        dist = get_distance(basee_point, p)
-        x = int(dist*np.cos(ang - angle_ + np.pi))
-        y = int(dist*np.sin(ang - angle_ + np.pi))
-        fixed_map[-y + 2*scale, 2*scale + x] = 0
-    
-    return (fixed_map, angle_)
-
-
-def get_angle(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return np.arctan2(y2-y1, x2-x1)
-
-def get_distance(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return np.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-def get_image_angle(lista):
-    angles = []
-    for i in range(len(lista)-4):
-        angle = get_angle(lista[i], lista[i+4])
-        angles.append(angle)
-    return angles
-
-def get_point_with_angle(lista, angle):
-    for i in range(len(lista)-4):
-        if get_angle(lista[i], lista[i+4]) == angle:
-            return [lista[i], lista[i+4]]
-
-def find_more_common_angle(lista):
-    return max(set(lista), key = lista.count)
